32284031ed663262ab6ebe32d0a0b9ecf931e3d5
Stefano Bianchi Mazzone <stefano.bianchimazzone@gmail.com>
2024-11-20 21:35:51 +0100

    Add commands and keymaps to interacts with LSPs

--- nvim/lua/sbm/plugins/telescope.lua
+++ nvim/lua/sbm/plugins/telescope.lua
@@ -29,6 +29,7 @@ return {
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
+    # Fuzzy Search
 		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
@@ -39,5 +40,14 @@ return {
 		keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Find keymaps" })
 		keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Search help documentation" })
 		keymap.set("n", "<leader>fm", "<cmd>Telescope commands<cr>", { desc = "Search available commands" })
-	end,
+
+    # Coding
+    keymap.set("n", "<leader>gd", "<cmd>Telescope lsp_definitions<cr>", { desc = "Go to definitions" })
+    keymap.set("n", "<leader>gs", "<cmd>Telescope lsp_document_symbols<cr>", { desc = "List document symbols" })
+    keymap.set("n", "<leader>gi", "<cmd>Telescope lsp_implementations<cr>", { desc = "Go to implementation" })
+    keymap.set("n", "<leader>gr", "<cmd>Telescope lsp_references<cr>", { desc = "Find references" })
+    keymap.set("n", "<leader>gt", "<cmd>Telescope lsp_type_definitions<cr>", { desc = "Go to type definition" })
+    keymap.set("n", "<leader>gI", "<cmd>Telescope lsp_incoming_calls<cr>", { desc = "Show incoming calls" })
+    keymap.set("n", "<leader>gO", "<cmd>Telescope lsp_outgoing_calls<cr>", { desc = "Show outgoing calls" })
+
 }
