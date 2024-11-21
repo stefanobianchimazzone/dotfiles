0d04f87a152f806e7d43e5be29c7896e90355f61
Stefano Bianchi Mazzone <stefano.bianchimazzone@gmail.com>
2024-11-21 05:34:34 +0100

    Add keybindings for moving line/block vertically

--- nvim/lua/sbm/core/keymaps.lua
+++ nvim/lua/sbm/core/keymaps.lua
@@ -2,21 +2,31 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert model with jk"})
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights"})
- 
+-- Bind ESC to jk in Insert mode
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert model with jk" })
+
+-- Move lines/blocks up and down
+keymap.set("n", "<C-j>", ":m .+1<CR>==", { desc = "Move line down" })
+keymap.set("n", "<C-k>", ":m .-2<CR>==", { desc = "Move line up" })
+keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv", { desc = "Move block down" })
+keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv", { desc = "Move block up" })
+
+-- Clear search results
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
 -- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", {desc = "Increment number"})
-keymap.set("n", "<leader>-", "<C-x>", {desc = "Decrement number"})
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
 -- Window management
-keymap.set("n", "<leader>sv", "<C-w>v", {desc = "Split window vertically"})
-keymap.set("n", "<leader>sh", "<C-w>s", {desc = "Split window horizontally"})
-keymap.set("n", "<leader>se", "<C-w>=", {desc = "Make splits equal in size"})
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", {desc = "Close current plit"})
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal in size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current plit" })
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", {desc = "Open new tab"})
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", {desc = "Close current tab"})
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", {desc = "Move to next tab"})
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", {desc = "Mover to previous tab"})
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", {desc = "Open current buffer in new tab"})
+-- Tabs management
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Move to next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Mover to previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
