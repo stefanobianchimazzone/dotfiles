f36c093d57939c8d8e5bfe66f467fb076173b328
Stefano Bianchi Mazzone <stefano.bianchimazzone@gmail.com>
2024-11-24 08:07:31 +0100

    Run linter

--- nvim/lua/sbm/lazy.lua
+++ nvim/lua/sbm/lazy.lua
@@ -1,22 +1,22 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ { import = "sbm.plugins" }, { import = "sbm.plugins.lsp" } }, {
-  checker = {
-    enable = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
+	checker = {
+		enable = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
